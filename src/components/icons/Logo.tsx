8147803193b69874eb57dc2a8e8f7c9683f3cd38
suleigolden <suleimamman@gmail.com>
2026-02-22 19:29:05 -0500

    feat: v2 logo

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -17,19 +17,7 @@ export const Logo: FC<LogoProps> = ({ size = 32 }) => {
       ml={{ base: 0, sm: 1 }}
       mb={{ base: 0, sm: 4 }}
     >
-      Sul
-      <Text
-        as={'span'}
-        color={colorMode === 'light' ? 'brand.500' : 'white'}
-        borderTop={colorMode === 'light' ? '2px #675dff solid' : '2px white solid'}
-        borderBottom={colorMode === 'light' ? '2px #675dff solid' : '2px white solid'}
-        borderRadius={9}
-        fontWeight={700}
-        p={'0 3px'}
-      >
-        Ber
-      </Text>
-      
+      SulBer
     </Text>
   );
 };
